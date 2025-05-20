cb7262f9cb551d4d7cef1c634adddae0950cdca6
Tiago Molero <tiagomolero@gmail.com>
2025-05-20 14:54:58 -0300

    Finalizado o MVP do sistema de controle financeiro/investimento

--- ControleDeInvestimentos/src/Main.java
+++
@@ -1,9 +0,0 @@
-import controller.LancamentoController;
-
-public class Main {
-
-    public static void main (String[] args){
-
-    }
-
-}
---
+++ ControleDeInvestimentos/src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import controller.LancamentoController;
+
+public class Main {
+
+    public static void main (String[] args){
+        LancamentoController controller = new LancamentoController();
+        controller.exibirMenu();
+    }
+
+}
