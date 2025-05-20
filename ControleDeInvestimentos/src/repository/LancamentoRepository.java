6fbb39e652f67806a9a8ce4d13108ee6823e7f8f
Tiago Molero <tiagomolero@gmail.com>
2025-05-20 12:36:15 -0300

    Criação dos Métodos da classe LançamentoRepository

---
+++ ControleDeInvestimentos/src/repository/LancamentoRepository.java
@@ -0,0 +1,32 @@
+package repository;
+
+import model.Lancamento;
+import model.TipoLancamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LancamentoRepository {
+
+    private List<Lancamento> lancamentos - new ArrayList<>();
+    private long proximoId = 1;
+
+    public void adicionar(Lancamento lancamento){
+        lancamento.setId(proximoId++);
+        lancamentos.add(lancamento);
+    }
+
+    public List<Lancamento> listarTodos(){
+        return new ArrayList<>(lancamentos);
+    }
+
+    public List<Lancamento> listarPorTipo(TipoLancamento tipo){
+        return lancamentos.stream().filter(l -> l.getTipo() == tipo).toList();
+    }
+
+    public void limparTodos(){
+        lancamentos.clear();
+        proximoId = 1;
+    }
+
+}
